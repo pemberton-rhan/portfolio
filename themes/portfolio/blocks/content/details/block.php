0fa506818ce6b51129b1998b28d9a0fca8537a9c
Rhan Pemberton <59292303+pemberton-rhan@users.noreply.github.com>
2023-06-02 12:07:03 -0500

    Contact form complete

--- themes/portfolio/blocks/content/details/block.php
+++ themes/portfolio/blocks/content/details/block.php
@@ -18,9 +18,20 @@
           <h3 class="section-title">
             <?php the_sub_field('title'); ?>
           </h3>
-          <div class="section-text">
-            <?php the_sub_field('text'); ?>
-          </div>
+          
+          <?php if( !get_sub_field('gravity_form') ): ?>
+            <div class="section-text txt-container-small">
+              <?php the_sub_field('text'); ?>
+            </div>
+          <?php else: ?>
+            <div id="g-form" class="section-form">
+              <?php 
+                $form_id = get_sub_field('gravity_form');
+                gravity_form( $form_id, false, false, false, '', false ); 
+              ?>
+            </div>
+          <?php endif; ?>
+          
         </section>
         
         <?php endwhile; ?>
